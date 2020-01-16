051744ae211788a754fd2c1be82b0762c69c760d
lavy1004 <33502802+lavy1004@users.noreply.github.com>
2020-01-16 14:44:18 +0900

    변동내용

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import Styled from 'styled-components/native';
+import Counter from './Screens/Counter';
 
-import React, { Fragment } from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
+const Container = Styled.View`
+  flex: 1;
+  background-color: #EEE;
+`;
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import Styled from 'styled-components/native'
-
-const ScrollView = Styled.ScrollView`
-  background-color:${Colors.white};
-`
-
-const Body = Styled.View`
-  background-color:${Colors.lighter};
-`
-
-const SectionContainer = Styled.View`
-  margin-top: 32px;
-  padding-horizontal:24px;
-`
-const SectionDescription = Styled.Text`
-  margin-top: 8px;
-  font-size: 18px;
-  font-weight: 400;
-  color: ${Colors.dark};
-
-`
-
-interface Props {}
-
-const App = ({}: Props) => {
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          >
-         <Header />
-         <Body>
-           <SectionContainer>
-             <SectionDescription>
-              step one
-             </SectionDescription>
-             <SectionDescription>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit expedita quas quam exercitationem porro eos culpa totam quo adipisci perferendis? Ea eveniet fugit ex delectus sapiente dolores autem et culpa?
-             </SectionDescription>
-           </SectionContainer>
-           <SectionContainer>
-             <SectionDescription>
-                See your
-              </SectionDescription>
-              <SectionDescription>
-                <ReloadInstructions/>
-              </SectionDescription>
-            </SectionContainer>
-         </Body>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <Container>
+      <Counter title="This is a Counter App" initValue={5} />
+    </Container>
   );
 };
-
 export default App;
